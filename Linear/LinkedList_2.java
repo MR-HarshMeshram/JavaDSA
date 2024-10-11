152375cbb2c8fcb851b28e6537229046cf277c71
Harsh <harshmeshram@gmail.com>
2024-10-11 11:15:15 +0530

    revers linkedlist

--- Linear/LinkedList_2.java
+++ Linear/LinkedList_2.java
@@ -10,7 +10,7 @@ class LinkedList_2{
         }
     }
     // add first
-    public static void AddFist(String data){
+    public  void AddFist(String data){
         Node new_node=new Node(data);
         if( head==null){
             head=new_node;
@@ -19,7 +19,7 @@ class LinkedList_2{
         new_node.next=head;
         head=new_node;
     }
-    public static void AddLast(String data){
+    public  void AddLast(String data){
         Node new_node=new Node(data);
         if(head==null){
             head=new_node;
@@ -33,7 +33,7 @@ class LinkedList_2{
         }
         current_node.next=new_node;
     }
-    public static void PrintList(){
+    public  void PrintList(){
         Node current_node=head;
         if(head==null){
             System.out.println(" linkedlist is empty ");
@@ -45,14 +45,14 @@ class LinkedList_2{
         }
         System.out.println(" null ");
     }
-     public static void DeleteFirst(){
+     public  void DeleteFirst(){
         if(head==null){
             System.out.println(" the list is empty ");
             return;
         }
         head=head.next;
     }
-    public static void DeleteLast(){
+    public  void DeleteLast(){
         if(head==null){
             System.out.println(" the List is empty ");
             return;
@@ -68,17 +68,32 @@ class LinkedList_2{
         current_node.next=null;
         
     }
+        public void Revers_LinkedList() {
+        if (head == null || head.next == null) {
+            return;  
+        }
+
+        Node prev_node = null;
+        Node current_node = head;
+
+        while (current_node != null) {
+            Node next_node = current_node.next;  
+            current_node.next = prev_node;  
+
+            
+            prev_node = current_node;
+            current_node = next_node;
+        }
+        head = prev_node;  
+    }
     public static void main(String[] args){
         LinkedList_2 LL=new LinkedList_2();
         LL.AddFist("FIRST");
         LL.AddFist("Second");
-        LL.PrintList();
-        LL.AddLast(" third ");
+        LL.AddFist(" third ");
         LL.PrintList();
 
-        LL.DeleteFirst();
-        LL.PrintList();
-        LL.DeleteLast();
+        LL.Revers_LinkedList();
         LL.PrintList();
     }
 }
